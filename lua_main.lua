72c771193048fce0a343cd15612d33819d63679b
ducthuong-dev <ducthuong343@gmail.com>
2025-07-21 10:25:48 +0700

    lua_main.lua

--- lua_main.lua
+++ lua_main.lua
@@ -9,16 +9,16 @@ local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/ducthuong-
 
 -- Danh sách GameID và Script tương ứng
 local games = {
-    ["2753915549"] = "games/blox_fruits.lua",            -- Blox Fruits
-    ["10324347967"] = "games/strongest_battlegrounds.lua", -- The Strongest Battlegrounds
-    ["13258801357"] = "games/grow_a_garden.lua",         -- Grow A Garden
-    ["1537690962"] = "games/ink_game.lua",               -- Ink Game
-    ["16034562004"] = "games/deal_rails.lua"             -- Deal Rails
+    ["2753915549"] = "blox.lua",            -- Blox Fruits
+    ["10324347967"] = "tsb.lua",            -- The Strongest Battlegrounds
+    ["13258801357"] = "garden.lua",         -- Grow A Garden
+    ["1537690962"] = "ink.lua",             -- Ink Game
+    ["16034562004"] = "deal.lua"            -- Deal Rails
 }
 
 local gameId = tostring(game.GameId)
 
--- Tự động tải script theo game hoặc mở tab chọn nếu không nhận diện được
+-- Tự động tải script theo game hoặc mở menu chọn nếu không nhận diện được
 if games[gameId] then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ducthuong-dev/lua.hub/main/" .. games[gameId]))()
 else
